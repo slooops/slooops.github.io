5385f655a9b9cbfdf05f089301d0c30172f2e576
Hanna Fernandez <hafernan@cisco.com>
2022-04-01 14:50:10 -0400

    CAB table has pages, started on CD compatibility

--- accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/controllers/ContractAssetBalanceController.java
+++ accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/controllers/ContractAssetBalanceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200") //TODO variable
+@CrossOrigin(origins = "${CORS_URL}")
 @RequestMapping("/api")
 public class ContractAssetBalanceController {
 
--- accruals-monitoring-ui/src/app/contract-asset-balance/contract-asset-balance.component.ts
+++ accruals-monitoring-ui/src/app/contract-asset-balance/contract-asset-balance.component.ts
@@ -8,6 +8,9 @@ import { ApiHttpService } from '../services/http.service';
   styleUrls: ['./contract-asset-balance.component.css']
 })
 export class ContractAssetBalanceComponent implements OnInit {  
+  @ViewChild('viewDetails', { static: true })
+  viewDetailsTemplate!: TemplateRef<any>;
+  
   tableOptions!: CuiTableOptions;
   cabTableData: any[] = [];
 
@@ -27,14 +30,17 @@ export class ContractAssetBalanceComponent implements OnInit {
   }
 
   getContractAssetBalance(): void {
-    this.http.get('contract-asset-balance').subscribe((data: any) => {
-      this.cabTableData = data;
-
-      // cabColumns.push(new CuiTableColumnOption({
-        
-      // }))
+    this.http.get('contract-asset-balance', {observe: 'response'}).subscribe((data: any) => {
+      console.log(data.headers);
+      this.cabTableData = data.body;
+      console.log(this.cabTableData);
+      
       let cabColumns: CuiTableColumnOption[] = [];
-      for (let column of Object.keys(data[0])) {
+      cabColumns.push(new CuiTableColumnOption({
+        name: '',
+        template: this.viewDetailsTemplate
+      }));
+      for (let column of Object.keys(this.cabTableData[0])) {
         cabColumns.push(new CuiTableColumnOption({
           'name': column,
           'sortable': true,
@@ -45,7 +51,8 @@ export class ContractAssetBalanceComponent implements OnInit {
       this.tableOptions = new CuiTableOptions({
         bordered: true,
         striped: true,
-        columns: cabColumns
+        columns: cabColumns,
+        dynamicData: false
       });
     });
   }
