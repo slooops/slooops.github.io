443596386f252a569a30028d440ce229093673cf
Hanna Fernandez <hafernan@cisco.com>
2022-04-05 15:32:29 -0400

    swapped localhost for env var in server

--- accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/controllers/RestApiController.java
+++ accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/controllers/RestApiController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200") //TODO variable
+@CrossOrigin(origins = "${CORS_URL}") //TODO variable
 public class RestApiController {
 
     @GetMapping("/health")
