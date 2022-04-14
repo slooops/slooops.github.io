c7eef336c4ff166db46238e233e1fbbc0acad7cb
Hanna Fernandez <hafernan@cisco.com>
2022-04-14 13:55:41 -0400

    started auth service

---
+++ accruals-monitoring-ui/src/app/providers/authentication.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  async getValidToken() {
+    
+  }
+
+}
