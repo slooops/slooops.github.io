b0a33305a5274ccf2e324c4aba6046b0796eddb3
Hanna Fernandez <hafernan@cisco.com>
2022-04-14 17:37:54 -0400

    added options to global cors methods

--- accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/configs/CorsConfig.java
+++ accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/configs/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(corsUrl).allowedMethods("*");
+                registry.addMapping("/api/**").allowedOrigins(corsUrl).allowedMethods("GET", "OPTIONS");
             }
         };
     }
