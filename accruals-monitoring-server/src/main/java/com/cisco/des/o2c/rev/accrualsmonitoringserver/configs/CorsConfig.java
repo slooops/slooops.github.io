01045e88e3ac5614fb9f2ddb4b1f4d00a79c33e4
Hanna Fernandez <hafernan@cisco.com>
2022-04-14 17:28:55 -0400

    trying to resolve cors issue

--- accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/configs/CorsConfig.java
+++ accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/configs/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(corsUrl);
+                registry.addMapping("/**").allowedOrigins(corsUrl).allowedMethods("*");
             }
         };
     }
