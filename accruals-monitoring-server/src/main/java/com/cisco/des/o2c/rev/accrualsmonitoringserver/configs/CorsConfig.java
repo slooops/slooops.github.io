dde4617e8024fab00bfab9fdde6fbf0ff84b68d4
Hanna Fernandez <hafernan@cisco.com>
2022-04-14 15:52:41 -0400

    added global CORS control

---
+++ accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/configs/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.cisco.des.o2c.rev.accrualsmonitoringserver.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@PropertySource("classpath:server.properties")
+public class CorsConfig {
+
+    @Value("${cors.url}")
+    public String corsUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(corsUrl);
+            }
+        };
+    }
+}
--- accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/controllers/RestApiController.java
+++ accruals-monitoring-server/src/main/java/com/cisco/des/o2c/rev/accrualsmonitoringserver/controllers/RestApiController.java
@@ -2,10 +2,12 @@ package com.cisco.des.o2c.rev.accrualsmonitoringserver.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "${CORS_URL}")
+@RequestMapping("/api")
 public class RestApiController {
 
     @GetMapping("/health")
