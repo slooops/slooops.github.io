31c7dcc99201ee52aa1ffb0b45087c7949b0b332
Hanna Fernandez <hafernan@cisco.com>
2022-05-17 15:08:41 -0400

    added filter structure to exception report

--- accruals-monitoring-ui/src/app/exception-report/exception-report.component.ts
+++ accruals-monitoring-ui/src/app/exception-report/exception-report.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExceptionReportComponent implements OnInit {
 
+  subId: string = '';
+  itemName: string = '';
+
+  trxStatus: string[] = ['Open', 'Fixed'];
+  selectedTrxStatus: string = 'Open';
+
+  ouName: string = '';
+  
+  startDate?: Date;
+  endDate?: Date;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubIdChange(subId: string): void {
+    this.subId = subId;
+    this.filterData();
+  }
+
+  onItemNameChange(itemName: string): void {
+    this.itemName = itemName;
+    this.filterData();
+  }
+
+  onTrxStatusChange(trxStatus: string): void {
+    this.selectedTrxStatus = trxStatus;
+    this.filterData();
+  }
+
+  onOuNameChange(ouName: string): void {
+    this.ouName = ouName;
+    this.filterData();
+  }
+
+  onStartDateChange(startDate: string): void {
+    this.startDate = new Date(startDate);
+    this.filterData();
+  }
+
+  onEndDateChange(endDate: string): void {
+    this.endDate = new Date(endDate);
+    this.filterData();
+  }
+
+  filterData(): void {
+
   }
 
 }
