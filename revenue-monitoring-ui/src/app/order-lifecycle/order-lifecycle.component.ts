9f1dbecfc5d6307a2bf34e955a6c872f2739ffb3
avudutha <avudutha@cisco.com>
2023-07-12 20:35:14 -0400

    order lifecycle changes

--- revenue-monitoring-ui/src/app/order-lifecycle/order-lifecycle.component.ts
+++ revenue-monitoring-ui/src/app/order-lifecycle/order-lifecycle.component.ts
@@ -58,6 +58,7 @@ export class OrderLifecycleComponent implements OnInit {
       this.dataSource = new MatTableDataSource<OrderLifecycleModel>(
         this.orderLifecycleStatus
       );
+      console.log(this.orderLifecycleStatus);
       this.filterData();
       this.length = this.orderLifecycleStatus.length;
       this.setSortAndPaginator();
@@ -106,48 +107,37 @@ export class OrderLifecycleComponent implements OnInit {
     });
   }
 
-  displayedColumns: string[] = [
+  displayedColumns = [
     'select',
     'PROGRAM_NAME',
     'ACCOUNT',
-    'SFDC_STATUS',
     'STATUS_AS_OF_DATE',
-    'DEAL_ID',
-    'SALES_ORDER',
     'EXPECTED_BOOK_DATE',
     'ACTUAL_BOOK_DATE',
-    // 'LINE_TYPE',
+    'AGING_BOOKING',
+    'AGING_HOLD_RELEASE',
+    'SALES_ORDER',
+    'ORDER_VALUE',
     'ORDER_STATUS',
     'INVOICING_STATUS',
     'REV_ACCR_STATUS',
     'GL_POSTING_STATUS',
     'HOLD_RELEASE_TARGET_DATE',
     'ACCRUALS_EXECUTION_TIME',
-    // 'CURRENCY',
     'ORDER_TOTAL',
-    'ORDER_TOTAL_USD',
     'TOTAL_CONTRAC_VALUE',
     'SUBSCRIPTION_ID',
     'INVOICE_DATE',
+    'BILLING_SCHEDULE',
     'INVOICE_AMOUNT',
-    'INVOICE_LINES',
-    // 'STATUS',
-    'COMMENTS',
-    'AGING_BOOKING',
-    'AGING_HOLD_RELEASE',
-    // 'LAST_UPDATE_DATE',
-    // 'ENABLED_FLAG',
-    // 'CURRENT_PERIOD_FLAG',
-    // 'WEB_ORDER_ID',
-    // 'QUOTE_NUMBER',
-    // 'CONTEXT',
-    // 'TOTAL_LINE_COUNT',
-    // 'HEADER_ID',
-    // 'ORDER_CREATION_DATE',
-    // 'ORDER_LEVEL_HOLD',
-    // 'LINE_LEVEL_HOLD',
-    // 'CUST_PO_NUMBER',
     'TERM_IN_YEARS',
+    'DEAL_ID',
+    'SFDC_STATUS',
+    'LINE_TYPE',
+    'TOTAL_LINE_COUNT',
+    'LINES_ON_HOLD',
+    'INVOICE_LINES',
+    'COMMENTS',
   ];
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
@@ -211,42 +201,31 @@ export class OrderLifecycleComponent implements OnInit {
 export interface OrderLifecycleModel {
   PROGRAM_NAME: string;
   ACCOUNT: string;
-  SFDC_STATUS: string;
   STATUS_AS_OF_DATE: string;
-  DEAL_ID: string;
-  SALES_ORDER: string;
   EXPECTED_BOOK_DATE: string;
   ACTUAL_BOOK_DATE: string;
-  // LINE_TYPE: string;
+  AGING_BOOKING: string;
+  AGING_HOLD_RELEASE: string;
+  SALES_ORDER: string;
+  ORDER_VALUE: string;
   ORDER_STATUS: string;
   INVOICING_STATUS: string;
   REV_ACCR_STATUS: string;
   GL_POSTING_STATUS: string;
   HOLD_RELEASE_TARGET_DATE: string;
   ACCRUALS_EXECUTION_TIME: string;
-  // CURRENCY: string;
   ORDER_TOTAL: string;
-  ORDER_TOTAL_USD: string;
   TOTAL_CONTRAC_VALUE: string;
   SUBSCRIPTION_ID: string;
   INVOICE_DATE: string;
+  BILING_SCHEDULE: string;
   INVOICE_AMOUNT: string;
-  INVOICE_LINES: string;
-  // STATUS: string;
-  COMMENTS: string;
-  AGING_BOOKING: string;
-  AGING_HOLD_RELEASE: string;
-  // LAST_UPDATE_DATE: string;
-  // ENABLED_FLAG: string;
-  // CURRENT_PERIOD_FLAG: string;
-  // WEB_ORDER_ID: string;
-  // QUOTE_NUMBER: string;
-  // CONTEXT: string;
-  // TOTAL_LINE_COUNT: string;
-  // HEADER_ID: string;
-  // ORDER_CREATION_DATE: string;
-  // ORDER_LEVEL_HOLD: string;
-  // LINE_LEVEL_HOLD: string;
-  // CUST_PO_NUMBER: string;
   TERM_IN_YEARS: string;
+  DEAL_ID: string;
+  SFDC_STATUS: string;
+  LINE_TYPE: string;
+  TOTAL_LINE_COUNT: string;
+  LINES_ON_HOLD: string;
+  INVOICE_LINES: string;
+  COMMENTS: string;
 }
