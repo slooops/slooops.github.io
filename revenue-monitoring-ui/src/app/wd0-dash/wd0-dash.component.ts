48756bc2d6838fad49b55edc83f2c92605ba696e
shwbahad <shwbahad@cisco.com>
2023-09-25 15:20:26 +0530

    wd0-dashboard UI changes version2

--- revenue-monitoring-ui/src/app/wd0-dash/wd0-dash.component.ts
+++ revenue-monitoring-ui/src/app/wd0-dash/wd0-dash.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-wd0-dash',
   templateUrl: './wd0-dash.component.html',
-  styleUrls: ['./wd0-dash.component.css']
+  styleUrls: ['./wd0-dash.component.css'],
 })
 export class Wd0DashComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  scrollToID(HTMLID: string): void {
+    document.getElementById(HTMLID).scrollIntoView();
   }
-
 }
