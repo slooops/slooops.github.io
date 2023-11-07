b13f6439ca99aed5b1a875827613c28d4128f1fa
avudutha <avudutha@cisco.com>
2023-11-07 17:11:34 -0500

    deploying to dev

--- revenue-monitoring-ui/node-proxy-server/server.js
+++ revenue-monitoring-ui/node-proxy-server/server.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Define the path for the proxy, e.g., '/api', and the target URL
+const proxyPath = "/api";
+const targetURL = "https://error-dashboard-dev-api.cisco.com"; // Target the same server as your Angular app
+
 app.use((req, res, next) => {
-  // Log request headers
-  console.log("Request Headers:", req.headers);
+  console.log("Request Headers from SSO:", req.headers);
   next();
 });
 
+// Create a proxy middleware for the API requests
+const apiProxy = createProxyMiddleware(proxyPath, {
+  target: targetURL,
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req) => {
+    // Log the incoming request headers from the SSO server
+    // console.log("Request Headers from SSO:", req.headers);
+  },
+});
+
+// Use the API proxy middleware
+app.use(proxyPath, apiProxy);
+
+// Start the proxy server
 app.listen(port, () => {
-  console.log(`Node.js server is running on port ${port}`);
+  console.log(`Proxy server is running on port ${port}`);
 });
