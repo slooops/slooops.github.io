172566e0a923cbe4fc133fd0df164e680a50e41b
avudutha <avudutha@cisco.com>
2023-11-21 14:54:06 -0500

    add axios

--- revenue-monitoring-ui/node-proxy-server/server.js
+++ revenue-monitoring-ui/node-proxy-server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
