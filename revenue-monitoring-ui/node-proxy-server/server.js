30c340c2788b7e57ef3125ee8273ef0ce8860f9d
avudutha <avudutha@cisco.com>
2024-03-22 14:56:37 -0400

    api url change

--- revenue-monitoring-ui/node-proxy-server/server.js
+++ revenue-monitoring-ui/node-proxy-server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 const proxyPath = "/api";
 
-const targetURL = "https://error-dashboard-api.cisco.com";
+const targetURL = "https://operations-dashboard-api.cisco.com";
 
 let hasMadePostRequest = false;
 
