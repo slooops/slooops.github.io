383cb450078e16022d1f85241876789d227d1ab0
avudutha <avudutha@cisco.com>
2024-04-14 13:41:18 +0530

    set user

--- revenue-monitoring-ui/node-proxy-server/server.js
+++ revenue-monitoring-ui/node-proxy-server/server.js
@@ -14,10 +14,17 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "../ui/dist")));
+app.use(express.static(path.join(__dirname, "../angular-app/dist")));
+
+// Define API endpoint
+app.get("/user/data", (req, res) => {
+  // Handle the request and send response
+
+  res.json({ auth_user: authUserName });
+});
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../ui/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../angular-app/dist", "index.html"));
 });
 
 app.listen(port, () => {
