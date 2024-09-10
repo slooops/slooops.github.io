fac7c9c7ed96e589d70adb51f4db4adcc88781da
Jack Sloop <jasloop@cisco.com>
2024-09-10 09:18:41 -0700

    changed DB connectivity to stage and added homepage changes and ui fixes for cms

--- revenue-monitoring-ui/angular-app/src/app/home/home.component.ts
+++ revenue-monitoring-ui/angular-app/src/app/home/home.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../providers/data.service';
 
+// import {
+//   AccessorModule,
+//   HbrButton,
+//   HbrInput,
+// } from '@harbor/elements-angular-standalone';
+// import {
+//   emptyState,
+//   magnifyingGlass,
+//   info,
+//   alertsPositiveLarge,
+// } from '@harbor/elements/icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  loading: boolean = false;
-
   constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
-    this.dataService.isLoading().subscribe((loading) => {
-      this.loading = loading;
-    });
-  }
+  ngOnInit(): void {}
 }
