be9e656b887f5fa052e4ef6806510274a3dc9ec6
jack <82819874+slooops@users.noreply.github.com>
2024-11-14 10:12:54 -0800

    o2c demo table complete

--- revenue-monitoring-ui/angular-app/src/app/o2c-demo/o2c-demo.component.ts
+++ revenue-monitoring-ui/angular-app/src/app/o2c-demo/o2c-demo.component.ts
@@ -1,43 +1,297 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ViewChildren,
+  QueryList,
+  ChangeDetectorRef,
+} from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-o2c-demo',
   templateUrl: './o2c-demo.component.html',
   styleUrl: './o2c-demo.component.css',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class O2cDemoComponent {
-  accrualsTotals: { [key: string]: number } = {
-    KAFKA_INBOUND_ERROR: 0,
-    KAFKA_INBOUND: 0,
-    ACCRUAL_LINEEXTN_BILLS_AHEAD_OF_TSV: 64.08,
-    ACCRUAL_PROCESS: 0,
-    ACCRUAL_DIST: 0,
-    ACCRUAL_SUMMARY: 0,
-    ACCRUAL_SUMM_DIST: 0,
-    KAFKA_PUBLISH: 0,
-    GL_BATCH_RECON: 0,
-  };
+  @ViewChild('outerSort', { static: true }) sort: MatSort;
+  @ViewChildren('innerSort') innerSort: QueryList<MatSort>;
+  @ViewChildren('innerTables') innerTables: QueryList<MatTable<Address>>;
 
-  // Define the steps array in a way that doesn’t modify the original `accrualsTotals`
-  skippedWords: string[] = ['IOL', 'AR', 'ID', 'GL', 'TSV'];
+  data: User[] = USERS;
 
-  formattedAccrualsSteps = Object.keys(this.accrualsTotals).map((key) => ({
-    label: this.formatLabel(key),
-    impact: this.accrualsTotals[key] || 'N/A',
-  }));
+  dataSource: MatTableDataSource<User>;
+  usersData: User[] = [];
+  columnsToDisplay = [
+    'common_attribute_between_all_tables_1',
+    'common_attribute_between_all_tables_2',
+    'common_attribute_between_all_tables_3',
+  ];
+  innerDisplayedColumns = ['Order_Summary', 'Order_ID', 'Order_Status'];
+  innerInnerDisplayedColumns = ['comment', 'commentStatus'];
+  expandedElement: User | null;
+  expandedElements: any[] = [];
 
-  // Function to format the label
-  formatLabel(label: string): string {
-    return label
-      .toLowerCase() // Convert to lowercase
-      .replace(/_/g, ' ') // Replace underscores with spaces
-      .split(' ') // Split into words
-      .map(
-        (word) =>
-          this.skippedWords.includes(word.toUpperCase())
-            ? word.toUpperCase() // Keep the word in uppercase if it's in skippedWords
-            : word.charAt(0).toUpperCase() + word.slice(1) // Capitalize the first letter otherwise
-      )
-      .join(' '); // Join words back with spaces
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    USERS.forEach((user) => {
+      if (
+        user.addresses &&
+        Array.isArray(user.addresses) &&
+        user.addresses.length
+      ) {
+        this.usersData = [
+          ...this.usersData,
+          { ...user, addresses: new MatTableDataSource(user.addresses) },
+        ];
+      } else {
+        this.usersData = [...this.usersData, user];
+      }
+    });
+    this.dataSource = new MatTableDataSource(this.usersData);
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.innerTables.forEach(
+      (table, index) =>
+        ((table.dataSource as MatTableDataSource<Address>).filter = filterValue
+          .trim()
+          .toLowerCase())
+    );
+  }
+
+  toggleRow(element: User) {
+    element.addresses &&
+    (element.addresses as MatTableDataSource<Address>).data.length
+      ? this.toggleElement(element)
+      : null;
+    this.cd.detectChanges();
+    this.innerTables.forEach(
+      (table, index) =>
+        ((table.dataSource as MatTableDataSource<Address>).sort =
+          this.innerSort.toArray()[index])
+    );
+  }
+
+  isExpanded(row: User): string {
+    const index = this.expandedElements.findIndex(
+      (x) =>
+        x.common_attribute_between_all_tables_1 ==
+        row.common_attribute_between_all_tables_1
+    );
+    if (index !== -1) {
+      return 'expanded';
+    }
+    return 'collapsed';
+  }
+
+  toggleElement(row: User) {
+    const index = this.expandedElements.findIndex(
+      (x) =>
+        x.common_attribute_between_all_tables_1 ==
+        row.common_attribute_between_all_tables_1
+    );
+    if (index === -1) {
+      this.expandedElements.push(row);
+    } else {
+      this.expandedElements.splice(index, 1);
+    }
+
+    //console.log(this.expandedElements);
   }
 }
+
+export interface User {
+  common_attribute_between_all_tables_1: string;
+  common_attribute_between_all_tables_2: string;
+  common_attribute_between_all_tables_3: string;
+  addresses?: Address[] | MatTableDataSource<Address>;
+}
+
+export interface Comment {
+  commenID: number;
+  comment: string;
+  commentStatus: string;
+}
+
+export interface Address {
+  Order_Summary: string;
+  Order_ID: string;
+  Order_Status: string;
+  comments?: Comment[] | MatTableDataSource<Comment>;
+}
+
+const USERS: User[] = [
+  {
+    common_attribute_between_all_tables_1: 'Order',
+    common_attribute_between_all_tables_2: 'cisco@test.com',
+    common_attribute_between_all_tables_3: '9864785214',
+    addresses: [
+      {
+        Order_Summary: 'Order Summary 1',
+        Order_ID: '78542',
+        Order_Status: 'Kansas',
+        comments: [
+          {
+            commenID: 1,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 2,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 3,
+            comment: 'Test',
+            commentStatus: 'Closed',
+          },
+        ],
+      },
+      {
+        Order_Summary: 'Order Summary 2',
+        Order_ID: '78554',
+        Order_Status: 'Texas',
+        comments: [
+          {
+            commenID: 4,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 5,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 6,
+            comment: 'Test',
+            commentStatus: 'Closed',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    common_attribute_between_all_tables_1: 'Subscription',
+    common_attribute_between_all_tables_2: 'deloitte@test.com',
+    common_attribute_between_all_tables_3: '8786541234',
+    addresses: [
+      {
+        Order_Summary: 'Order_Summary 5',
+        Order_ID: '23547',
+        Order_Status: 'Utah',
+        comments: [
+          {
+            commenID: 7,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 8,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 9,
+            comment: 'Test',
+            commentStatus: 'Closed',
+          },
+        ],
+      },
+      {
+        Order_Summary: 'Order_Summary 5',
+        Order_ID: '23547',
+        Order_Status: 'Ohio',
+        comments: [
+          {
+            commenID: 19,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 11,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 12,
+            comment: 'Test',
+            commentStatus: 'Closed',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    common_attribute_between_all_tables_1: 'Invoice',
+    common_attribute_between_all_tables_2: 'happyHolidays@test.com',
+    common_attribute_between_all_tables_3: '7856452187',
+    addresses: [
+      {
+        Order_Summary: 'Order_Summary 5',
+        Order_ID: '23547',
+        Order_Status: 'Utah',
+        comments: [
+          {
+            commenID: 13,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 14,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 15,
+            comment: 'Test',
+            commentStatus: 'Closed',
+          },
+        ],
+      },
+      {
+        Order_Summary: 'Order_Summary 5',
+        Order_ID: '23547',
+        Order_Status: 'Ohio',
+        comments: [
+          {
+            commenID: 16,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 17,
+            comment: 'Test',
+            commentStatus: 'Open',
+          },
+          {
+            commenID: 18,
+            comment: 'Test',
+            commentStatus: 'Closed',
+          },
+        ],
+      },
+    ],
+  },
+];
