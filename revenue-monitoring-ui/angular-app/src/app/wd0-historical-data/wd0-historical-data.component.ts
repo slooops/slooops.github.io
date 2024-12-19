b5e407b3cdc1b36ad2afcf0a73c15dc59f4bbc68
jack <82819874+slooops@users.noreply.github.com>
2024-12-19 13:07:58 -0700

    rollover time set change

--- revenue-monitoring-ui/angular-app/src/app/wd0-historical-data/wd0-historical-data.component.ts
+++ revenue-monitoring-ui/angular-app/src/app/wd0-historical-data/wd0-historical-data.component.ts
@@ -89,10 +89,10 @@ export class Wd0HistoricalDataComponent implements OnInit {
       const wd1 = new Date(monthEnd);
 
       wd3.setDate(monthEnd.getDate() - 3);
-      wd3.setHours(6); // 4 PM DST, 3 PM summer time rollover for WD-3
+      wd3.setHours(15); // 4 PM DST, 3 PM summer time rollover for WD-3
 
       wd2.setDate(monthEnd.getDate() - 2);
-      wd2.setHours(15); // 4 PM rollover for WD-2
+      wd2.setHours(5); // 4 PM rollover for WD-2
 
       wd1.setDate(monthEnd.getDate() - 1);
       wd1.setHours(15); // 4 PM rollover for WD-1
