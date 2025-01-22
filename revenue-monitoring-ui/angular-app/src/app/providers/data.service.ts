9e0e9893536ae282e6f56f47e21c613f25b7cc34
Abhijith Bharadwaj Vuduthala <avudutha@cisco.com>
2025-01-21 22:11:03 -0600

    test

--- revenue-monitoring-ui/angular-app/src/app/providers/data.service.ts
+++ revenue-monitoring-ui/angular-app/src/app/providers/data.service.ts
@@ -74,19 +74,28 @@ export class DataService implements OnDestroy {
     return this.fetchWithCache('monitoring-period-status', destroyManager);
   }
 
+  private i2cSummary$: Observable<any>;
   getI2CSummary(destroyManager: DestroyManager): Observable<any> {
-    return this.fetchWithCache('invoice-to-cash-summary', destroyManager);
+    if (!this.i2cSummary$) {
+      this.i2cSummary$ = this.http
+        .get('invoice-to-cash-summary', destroyManager)
+        .pipe(shareReplay(1), takeUntil(this.destroy$));
+    }
+    return this.i2cSummary$;
   }
 
   getLargeDealData(destroyManager: DestroyManager): Observable<any> {
     return this.fetchWithCache('order-status', destroyManager);
   }
 
+  private userRoles$: Observable<any>;
   getRoles(username: string, destroyManager: DestroyManager): Observable<any> {
-    return this.fetchWithCache(
-      `user-role?username=${username}`,
-      destroyManager
-    );
+    if (!this.userRoles$) {
+      this.userRoles$ = this.http
+        .get(`user-role?username=${username}`, destroyManager)
+        .pipe(shareReplay(1), takeUntil(this.destroy$));
+    }
+    return this.userRoles$;
   }
 
   getUserRoles() {
