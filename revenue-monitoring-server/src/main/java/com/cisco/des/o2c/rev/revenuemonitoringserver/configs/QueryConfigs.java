24fab32e42809d457e4f09efea03b2bdbcad1695
Abhijith Bharadwaj Vuduthala <avudutha@cisco.com>
2025-04-20 21:40:02 -0400

    test

--- revenue-monitoring-server/src/main/java/com/cisco/des/o2c/rev/revenuemonitoringserver/configs/QueryConfigs.java
+++ revenue-monitoring-server/src/main/java/com/cisco/des/o2c/rev/revenuemonitoringserver/configs/QueryConfigs.java
@@ -941,7 +941,6 @@ public class QueryConfigs {
 
     @Bean( name = "issueReportingDashSummary" )
     public String getIssueReportingDashSummary() {return this.issueReportingDashSummary; }
-    
 
     @Bean( name = "rpoExtractSummary" )
     public String getRpoExtractSummary() {return this.rpoExtractSummary; }
