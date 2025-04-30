d80f9f9fd633070ef9f161463f056f1901b0c1c0
Abhijith Bharadwaj Vuduthala <avudutha@cisco.com>
2025-04-30 17:50:53 -0400

    changed config url

--- revenue-monitoring-ui/angular-app/src/app/providers/authentication.service.ts
+++ revenue-monitoring-ui/angular-app/src/app/providers/authentication.service.ts
@@ -32,7 +32,7 @@ export class AuthenticationService {
   async getTokens() {
     let authClientId = this.appConfig.getConfig().authClientId;
     let authClientSecret = this.appConfig.getConfig().authClientSecret;
-    let ssoUrl = 'https://cloudsso-test.cisco.com';
+    let ssoUrl = 'https://int-id.cisco.com';
 
     const href = window.location.href;
     if (
@@ -41,7 +41,7 @@ export class AuthenticationService {
       href.search('-ts3') !== -1 ||
       href.search('-int') !== -1
     ) {
-      ssoUrl = 'https://cloudsso-test.cisco.com';
+      ssoUrl = 'https://int-id.cisco.com';
     } else if (href.search('localhost') !== -1) {
       ssoUrl = '';
     }
