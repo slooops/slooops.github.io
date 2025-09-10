9de1883b479fb5e0b693b031888240da95ce97f7
Abhijith Bharadwaj Vuduthala <avudutha@cisco.com>
2025-09-10 11:18:19 -0400

    disbaled o2c360 in stage

--- revenue-monitoring-ui/angular-app/src/app/business-insights/business-insights.component.ts
+++ revenue-monitoring-ui/angular-app/src/app/business-insights/business-insights.component.ts
@@ -80,11 +80,11 @@ export class BusinessInsightsComponent implements OnInit {
       component: 'app-issue-reporting',
       role: ['ADMIN', 'ISSUE_RESOLUTION', 'ISSUE_APPROVAL'],
     },
-    {
-      label: 'O2C - 360',
-      component: 'app-o2c-360',
-      role: ['ADMIN', 'O360'],
-    },
+    // {
+    //   label: 'O2C - 360',
+    //   component: 'app-o2c-360',
+    //   role: ['ADMIN', 'O360'],
+    // },
   ];
 
   selectedIndex: number = 0;
