dbde775f6d1bc79cc6193fdbb30b6e64c08b0122
jasloop_cisco <jasloop@cisco.com>
2025-12-23 10:57:41 -0800

    error component fix attempt 2

--- revenue-monitoring-ui/angular-app/src/app/error/error.component.ts
+++ revenue-monitoring-ui/angular-app/src/app/error/error.component.ts
@@ -2,19 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-error',
-  template: `
-    <div class="image-container">
-      <img src="assets/thumbnails/disconnected.svg" alt="Access Restricted" />
-    </div>
-
-    <div class="error-page">
-      <h1 class="error-title"><b>Access Restricted</b></h1>
-      <p class="error-text">
-        If you need access to this dashboard, please request it from the Invoice
-        to Cash team.
-      </p>
-    </div>
-  `,
+  templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss'],
   standalone: true,
   imports: [],
