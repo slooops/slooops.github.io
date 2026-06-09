0ede825c5b542bceec4cc94faae4811e0fce70c9
Abhijith Bharadwaj Vuduthala <avudutha@cisco.com>
2026-06-09 10:10:35 -0400

    agents tally updated

--- revenue-monitoring-ui/angular-app/src/app/esp/esp-home/caseiq/caseiq.component.ts
+++ revenue-monitoring-ui/angular-app/src/app/esp/esp-home/caseiq/caseiq.component.ts
@@ -128,10 +128,10 @@ export class CaseiqComponent implements AfterViewInit, OnDestroy, OnChanges {
 
   // Resolution Agents Deployed per team
   resolutionAgents: { team: string; deployed: number; total: number }[] = [
-    { team: 'Finance IT', deployed: 89, total: 90 },
+    { team: 'Finance IT', deployed: 90, total: 90 },
     { team: 'OM', deployed: 14, total: 14 },
     { team: 'SM', deployed: 11, total: 11 },
-    { team: 'I2C', deployed: 18, total: 19 },
+    { team: 'I2C', deployed: 19, total: 19 },
     { team: 'AIT', deployed: 11, total: 11 },
     { team: 'FPP', deployed: 14, total: 14 },
     { team: 'P2P', deployed: 10, total: 10 },
