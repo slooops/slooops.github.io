9f80d207c7e938c5de183f3726975c759156c26f
Abhijith Bharadwaj Vuduthala <avudutha@cisco.com>
2026-07-30 20:05:31 -0400

    test case fix for build fix

--- revenue-monitoring-server/src/test/java/com/cisco/des/o2c/rev/revenuemonitoringserver/services/EspCaseManagerServiceTest.java
+++ revenue-monitoring-server/src/test/java/com/cisco/des/o2c/rev/revenuemonitoringserver/services/EspCaseManagerServiceTest.java
@@ -31,7 +31,7 @@ class EspCaseManagerServiceTest {
                 "q11", "q12", "q13", "q14", "q15", "q16", "q17",
                 "q18", "q19", "q20", "q21", "q22", "q23", "q24",
                 "q25", "q26", "q27", "q28", "q29", "q30", "q31",
-                "q32", "q33", "q34", "q35","q36");
+                "q32", "q33", "q34", "q35","q36", "q37");
         Field f = EspCaseManagerService.class.getDeclaredField("common");
         f.setAccessible(true);
         f.set(service, common);
